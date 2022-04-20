09ec315226323428d9aad54f21ccbf0876ac05bb
Rusbear007 <102467010+Rusbear007@users.noreply.github.com>
2022-04-20 11:38:52 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+const board = document.querySelector('#board')
+const colors = ['#9933CC', '#00FF33', '#CCFF00', '#99FFFF', '#CC33FF']
+const SQUARES_NUMBER = 2000
+
+for (let i = 0; i< SQUARES_NUMBER; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover',  setColor)
+
+    square.addEventListener('mouseleave',  removeColor)
+    
+    board.append(square)
+}
+
+function setColor(event) {
+const element = event.target
+const color = getRandomColor()    
+element.style.backgroundColor = color
+element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(event) {
+    const element = event.target
+    element.style.backgroundColor = '#1d1d1d'
+    element.style.boxShadow = `0 0 2px #000`
+}
+
+function getRandomColor () {
+    return colors[ Math.floor(Math.random() * colors.length)]
+}
